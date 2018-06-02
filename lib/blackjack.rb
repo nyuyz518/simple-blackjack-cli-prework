6323b79d3b448e1cfea1a8da2bef56487c32aae7
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-02 18:01:06 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,26 +7,23 @@ end
 def deal_card
   # code #deal_card here
   return rand(1..11)
-end
-
+end 
 
 def prompt_user
   # code #prompt_user here
   puts "Type 'h' to hit or 's' to stay"
 end
   
-  
 def display_card_total (card_total)
   # code #display_card_total here
   puts "Your cards add up to #{card_total}"
 end
 
-
-
 def get_user_input
   # code #get_user_input here
   gets.chomp 
 end
+
 
 def end_game (card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
