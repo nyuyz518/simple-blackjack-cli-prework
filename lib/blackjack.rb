95103ba6deea722111cea0f4126e5c9e3443e5de
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-02 18:15:05 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -63,7 +63,8 @@ end
 def runner
   # code runner here
   welcome
-  card_total += initial_round
+  initial_round
+
   until card_total > 21 do
      card_total = hit? (card_total)
      display_card_total(card_total)
